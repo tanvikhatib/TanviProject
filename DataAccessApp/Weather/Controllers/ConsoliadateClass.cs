3d649fd8078f955e9eb9efc28acc246db6c97bb3
heman <hemanthkumar.m13@gmail.com>
2021-04-22 17:24:00 -0400

    Add project files.

---
+++ DataAccessApp/Weather/Controllers/ConsoliadateClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Weather.Controllers
+{
+    public class ConsoliadateClass
+    {
+        public List<Weather> consolidated_weather { get; set; }
+    }
+}
---
+++ DataAccessApp/Weather/Models/WeatherClass.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Weather.Models
+{
+    public class WeatherClass
+    {
+        public int id { get; set; }
+        public string weather_state_name { get; set; }
+        public string weather_state_abbr { get; set; }
+        public string wind_direction_compass { get; set; }
+        public DateTime created { get; set; }
+        public DateTime applicable_date { get; set; }
+        public float min_temp { get; set; }
+        public float max_temp { get; set; }
+        public float the_temp { get; set; }
+        public float wind_speed { get; set; }
+        public float wind_direction { get; set; }
+        public float air_pressure { get; set; }
+        public float humidity { get; set; }
+        public float visibility { get; set; }
+        public float predicability { get; set; }
+    }
+
+
+
+}
